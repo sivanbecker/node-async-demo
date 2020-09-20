68c6929662d0b2eaf111a598fcb1e4c55f6c346e
Sivan Becker <sivanbecker@gmail.com>
2020-09-20 16:28:09 +0300

    simple callback example

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ getUser(1, (user) => {
     console.log('User', user)
     getRepos(user.githubUsername, (repos) => {
         console.log('Repos', repos)
+        getCommits(repos[0], (commits) => {
+            console.log(commits)
+        });
     });
 
 });
@@ -19,5 +22,11 @@ function getRepos(username, callback) {
     setTimeout(() => {
         console.log('Getting Repos from GitHub...');
         callback(['repo1', 'repo2', 'repo4']);
+    }, 2000);
+}
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        console.log('Getting Commits for repo ' + repo);
+        callback(['commit1', 'commit2', 'commit3']);
     }, 2000);
 }
